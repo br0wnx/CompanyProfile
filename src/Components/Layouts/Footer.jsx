4ed3f13fcf6418cb15cbf1975dd9e6db3a496768
br0wnx <annisar962@gmail.com>
2025-10-31 16:30:51 +0700

    import link

--- src/Components/Layouts/Footer.jsx
+++ src/Components/Layouts/Footer.jsx
@@ -1,5 +1,6 @@
 import { FaInstagram, FaLinkedin, FaFacebook } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   const navigate = useNavigate();
