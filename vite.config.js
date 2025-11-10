86fdfdd646b9e8fbf5be4cd24594599b4e9a16a8
br0wnx <annisar962@gmail.com>
2025-11-10 09:06:01 +0700

    add routes

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       name: 'vite-plugin-generate-sitemap',
       apply: 'build',
       async closeBundle() {
-        const routes = ['/', '/about', '/contact']
+        const routes = ['/', '/about', '/services', '/portal', '/contact', '/contact#faq', '/services#tariff']
         const smStream = new SitemapStream({ hostname: SITE_URL })
 
         routes.forEach((route) => {
